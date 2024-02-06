8b030cdf1442c41e67e6d8e0044c99c18cc31b66
HU Qiwen <huqiwen1023@163.com>
2024-02-06 23:16:17 +0800

    day3

---
+++ 2015_day3.py
@@ -0,0 +1,54 @@
+def day3_part1(input_text):
+    x = 0
+    y = 0
+    houses = []
+    for s in input_text:
+        if s == '^':
+            x += 1
+        elif s == 'v':
+            x -= 1
+        elif s == '>':
+            y += 1
+        else:
+            y -= 1
+        houses.append((x,y))
+    return len(set(houses))
+
+def day3_part2(input_text):
+    x1 = 0
+    y1 = 0
+    x2 = 0
+    y2 = 0
+    step = 0
+    houses = []
+    for s in input_text:
+        step += 1
+        if step % 2 ==1:
+            if s == '^':
+                x1 += 1
+            elif s == 'v':
+                x1 -= 1
+            elif s == '>':
+                y1 += 1
+            else:
+                y1 -= 1
+            houses.append((x1,y1))
+        else:
+            if s == '^':
+                x2 += 1
+            elif s == 'v':
+                x2 -= 1
+            elif s == '>':
+                y2 += 1
+            else:
+                y2 -= 1
+            houses.append((x2,y2))
+    return len(set(houses))
+
+
+if __name__ == '__main__':
+    with open('day_03.txt', 'r') as file_obj:
+        input_text = file_obj.read()
+    #print(input_text)
+    print(day3_part1(input_text))
+    print(day3_part2(input_text))
